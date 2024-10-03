d5b32a613bda14efa470dab184038f8d8746b0d0
Ra998181 <ramanandp48@gmail.com>
2024-10-03 14:30:52 +0530

    Its a turnary file

---
+++ turnary.js
@@ -0,0 +1,11 @@
+let age = 69;
+
+if (age > 16) {
+  if (age <= 60) {
+    console.log("Eligible for vote");
+  } else {
+    console.log("Failed");
+  }
+} else {
+  console.log("Enter valid number");
+}
